94d0a97b51495c8ceb3ab46709cdfcfa08422239
java2124 <rafabf1508@gmail.com>
2021-10-17 19:39:26 -0300

    JOption pane para receber os nomes

--- BuscaDeClientes/AppBuscaClientes.java
+++ BuscaDeClientes/AppBuscaClientes.java
@@ -6,12 +6,11 @@
 package BuscaDeClientes;
 
 import java.util.ArrayList;
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class AppBuscaClientes {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         String nomeCliente = "";
         int i = 0;
         ArrayList <Cliente> clientes = new ArrayList<>();
@@ -21,37 +20,22 @@ public class AppBuscaClientes {
         clientes = dados.dadosClientes();
         clientes = cliente.ordenacaoLista(clientes);
 
-        System.out.println("Olá! Seja Bem-Vindo(a) ao sistema de clientes da empresa Anhembi Morumbi!");
+        JOptionPane.showMessageDialog(null, "Olá! Seja Bem-Vindo(a) ao sistema de clientes da empresa Anhembi Morumbi!");
 
-        do{
-            System.out.println();    
-            System.out.println("Digite o nome do cliente que deseja procurar (Se quiser sair do sistema basta digitar 'exit')");
-            nomeCliente = scanner.nextLine();
+        do{   
+            nomeCliente = JOptionPane.showInputDialog("Digite o nome do cliente que deseja procurar (Se quiser sair do sistema basta digitar 'exit')");
 
             if(nomeCliente.equals("exit")){
                 i = 1;
             } else {
-                String [] resultado = cliente.pesquisarCliente(clientes, "Fábio Lima");
+                String [] resultado = cliente.pesquisarCliente(clientes, nomeCliente);
     
                 if(resultado[2] != null){
-                    System.out.println(); 
-                    System.out.println("Nome: " + resultado[2]);
-                    System.out.println("Sexo: " + resultado[3]);
-                    System.out.println("Endereço: " + resultado[4]);
-                    System.out.println("Estado: " + resultado[5]);
-                    System.out.println("Email: " + resultado[6]);
-                    System.out.println("Telefone: " + resultado[7]);
-                    System.out.println("Idade: " + resultado[8]);
-                    System.out.println();
-                    System.out.println(resultado[1]);
+                    JOptionPane.showMessageDialog(null, "Nome: " + resultado[2] + "\n" + "Sexo: " + resultado[3] + "\n" + "Endereço: " + resultado[4] + "\n" + "Estado: " + resultado[5] + "\n" + "Email: " + resultado[6] + "\n" + "Telefone: " + resultado[7] + "\n" + "Idade: " + resultado[8] + "\n" + "\n" + resultado[1]);
                 } else {
-                    System.out.println(); 
-                    System.out.println(resultado[0]);
-                    System.out.println(resultado[1]);
+                    JOptionPane.showMessageDialog(null, resultado[0] + "\n" + "\n" + resultado[1]);
                 }
             }
         } while(i==0);   
-
-        scanner.close();
     }
 }
